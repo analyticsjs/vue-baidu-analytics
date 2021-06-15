87d46b37cdd5089668215015c96f623d5da6f4b7
程沛权 <chengpeiquan@chengpeiquan.com>
2021-06-15 15:22:46 +0800

    chore: test git hooks

--- .eslintrc.ts
+++ .eslintrc.ts
@@ -1,14 +1,22 @@
 module.exports = {
   root: true,
   env: {
-    browser: true
+    node: true,
+    browser: true,
   },
+  extends: [
+    'prettier',
+  ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaVersion: 2020
+    ecmaVersion: 2020,
   },
   plugins: [
-    '@typescript-eslint'
+    '@typescript-eslint',
   ],
-  rules: {}
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'prettier/prettier': 'warn',
+  }
 };
